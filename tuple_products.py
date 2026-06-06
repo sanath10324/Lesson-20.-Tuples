72d9e6b87a9a34e7264e83216667d099f230701e
sanath10324 <sanathkatkamgamer@gmail.com>
2026-06-06 14:27:11 +0400

    Tuple Product

---
+++ tuple_products.py
@@ -0,0 +1,13 @@
+tup1 = (4, 3, 2, 2, -1, 18)
+tup2 = (2, 4, 8, 8, 3, 2, 9)
+
+product1 = 1
+for num in tup1:
+    product1 *= num 
+
+product2 = 1
+for num in tup2:
+    product2 *= num 
+
+print("The Product of Tuple1 is:", product1)
+print("The product of Tuple2 is:", product2)
